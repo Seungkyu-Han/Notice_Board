54e15c0cd8737be44869833efbabf93fcbdc1251
Seungkyu-Han <trust1204@gmail.com>
2024-10-26 19:19:32 +0900

    feat: add coroutine and javax dependency

--- security/build.gradle.kts
+++ security/build.gradle.kts
@@ -12,6 +12,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security:3.3.5")
 
     implementation("io.jsonwebtoken:jjwt:0.9.1")
+
+    implementation("org.glassfish.jaxb:jaxb-runtime:2.3.2")
+
+    runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.9.0")
+    runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.9.0")
+
+    runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-rx3:1.9.0")
 }
 
 tasks.test {
