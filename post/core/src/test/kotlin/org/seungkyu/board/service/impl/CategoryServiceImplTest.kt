7c860a33f1ba1307aeb868d2ac4bcafc29eaf96f
Seungkyu-Han <trust1204@gmail.com>
2024-10-27 22:46:29 +0900

    test: add category get api

--- post/core/src/test/kotlin/org/seungkyu/board/service/impl/CategoryServiceImplTest.kt
+++ post/core/src/test/kotlin/org/seungkyu/board/service/impl/CategoryServiceImplTest.kt
@@ -1,6 +1,10 @@
 package org.seungkyu.board.service.impl
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
 import kotlinx.coroutines.reactor.mono
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -12,16 +16,25 @@ import org.mockito.kotlin.any
 import org.seungkyu.board.config.SeungkyuAuthentication
 import org.seungkyu.board.data.enums.Role
 import org.seungkyu.board.dto.req.CategoryPostReq
+import org.seungkyu.board.dto.res.CategoryGetRes
 import org.seungkyu.board.entity.CategoryDocument
 import org.seungkyu.board.repository.CategoryMongoRepository
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.HttpStatus
+import org.springframework.http.codec.HttpMessageWriter
+import org.springframework.mock.http.server.reactive.MockServerHttpRequest
+import org.springframework.mock.web.server.MockServerWebExchange
 import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.web.reactive.function.server.HandlerStrategies
 import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.result.view.ViewResolver
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
+import java.util.*
 
 @ExtendWith(SpringExtension::class)
 @ContextConfiguration(classes = [CategoryServiceImpl::class])
@@ -35,6 +48,8 @@ class CategoryServiceImplTest{
     @Mock
     private lateinit var serverRequest: ServerRequest
 
+    private val objectMapper = ObjectMapper()
+
     @BeforeEach
     fun setUp() {
         MockitoAnnotations.openMocks(this)
@@ -44,7 +59,7 @@ class CategoryServiceImplTest{
     @Nested
     inner class PostCategory{
         private val testCategory = CategoryDocument(
-            id = null, name = "testName", isAscending = true, searchCount = 0
+            id = null, name = "testName", isAscending = true, searchCount = 0, userId = "testUser"
         )
 
         @Test
@@ -114,6 +129,62 @@ class CategoryServiceImplTest{
 
     }
 
+    @Nested
+    inner class GetCategory{
+        @Test
+        fun get_return_ok(){
+            mono{
+                //given
+                val category1 = CategoryDocument(
+                    id = null, name = "testName1", isAscending = true, userId = "testUser", searchCount = 1
+                )
+
+                val category2 = CategoryDocument(
+                    id = null, name = "testName2", isAscending = true, userId = "testUser", searchCount = 2
+                )
+
+                `when`(categoryMongoRepository.findAll())
+                    .thenReturn(
+                        Flux.fromIterable(
+                            listOf(
+                                category1, category2
+                            )
+                        )
+                    )
+
+                //when
+                val target = Mono.just(categoryServiceImpl.get(serverRequest))
+
+                val extractedCategoryGet:ArrayList<CategoryGetRes> = fetchBody<ArrayList<CategoryGetRes>>(target.awaitSingle())
+
+                Assertions.assertEquals(2, extractedCategoryGet.size)
+
+                StepVerifier.create(target)
+                    .expectNextMatches {
+                        HttpStatus.OK == it.statusCode()
+                    }
+                    .verifyComplete()
+            }.block()
+
+        }
+    }
 
 
+    private suspend inline fun <reified T> fetchBody(serverResponse: ServerResponse): T {
+        val defaultContext: ServerResponse.Context = object : ServerResponse.Context {
+            override fun messageWriters(): List<HttpMessageWriter<*>> {
+                return HandlerStrategies.withDefaults().messageWriters()
+            }
+
+            override fun viewResolvers(): List<ViewResolver> {
+                return Collections.emptyList()
+            }
+        }
+
+        val request = MockServerHttpRequest.get("https://github.com/Seungkyu-Han").build()
+        val exchange = MockServerWebExchange.from(request)
+        serverResponse.writeTo(exchange, defaultContext).awaitSingleOrNull()
+        val response = exchange.response.bodyAsString.awaitSingle()
+        return objectMapper.readValue(response, T::class.java)
+    }
 }
