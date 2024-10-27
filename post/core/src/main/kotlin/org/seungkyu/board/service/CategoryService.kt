026f14296fec020051ac2edefad794a4beef9bb2
Seungkyu-Han <trust1204@gmail.com>
2024-10-27 22:09:49 +0900

    feat: add category post api

---
+++ post/core/src/main/kotlin/org/seungkyu/board/service/CategoryService.kt
@@ -0,0 +1,9 @@
+package org.seungkyu.board.service
+
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+
+interface CategoryService {
+
+    suspend fun post(serverRequest: ServerRequest): ServerResponse
+}
---
+++ post/core/src/main/kotlin/org/seungkyu/board/service/impl/CategoryServiceImpl.kt
@@ -0,0 +1,54 @@
+package org.seungkyu.board.service.impl
+
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.seungkyu.board.data.enums.Role
+import org.seungkyu.board.dto.req.CategoryPostReq
+import org.seungkyu.board.entity.CategoryDocument
+import org.seungkyu.board.repository.CategoryMongoRepository
+import org.seungkyu.board.service.CategoryService
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.buildAndAwait
+import reactor.core.publisher.Mono
+
+@Service
+class CategoryServiceImpl(
+    private val categoryMongoRepository: CategoryMongoRepository
+) : CategoryService{
+
+    override suspend fun post(serverRequest: ServerRequest): ServerResponse {
+        if(Role.ADMIN.name != getUserRoleByContext().awaitSingleOrNull()){
+            return ServerResponse.status(403).buildAndAwait()
+        }
+
+        val categoryPostReq = serverRequest.bodyToMono(CategoryPostReq::class.java).awaitSingle()
+
+        val categoryDocument = CategoryDocument(
+            id= null,
+            name = categoryPostReq.name,
+            isAscending = categoryPostReq.isAscending,
+            searchCount = 0
+        )
+
+        categoryMongoRepository.save(categoryDocument).awaitSingle()
+
+        return ServerResponse.status(201).buildAndAwait()
+    }
+
+    private fun getUserIdByContext(): Mono<String> {
+        return ReactiveSecurityContextHolder.getContext()
+            .map {
+                it.authentication.name
+            }
+    }
+
+    private fun getUserRoleByContext(): Mono<String> {
+        return ReactiveSecurityContextHolder.getContext()
+            .map{
+                it.authentication.authorities.first().authority
+            }
+    }
+}
