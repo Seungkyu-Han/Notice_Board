badbb4000992e5418bd4ddb620c901b38d4672d8
Seungkyu-Han <trust1204@gmail.com>
2024-10-27 22:09:19 +0900

    feat: add category repository

---
+++ post/persistence/src/main/kotlin/org/seungkyu/board/entity/CategoryDocument.kt
@@ -0,0 +1,14 @@
+package org.seungkyu.board.entity
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "Category")
+data class CategoryDocument(
+    @Id
+    val id: ObjectId?,
+    var name: String,
+    var isAscending: Boolean,
+    var searchCount: Int,
+)
