0ee940b97a6d19dd768459279580423d0d8fc35f
Seungkyu-Han <trust1204@gmail.com>
2024-11-04 14:41:58 +0900

    feat: add gateway dependencies

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
-    kotlin("jvm")
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "seungkyu"
@@ -10,7 +13,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 }
 
 tasks.test {
